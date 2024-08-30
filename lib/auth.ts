9ecea5220d56959156e4ee28409ed4ec7fdb1813
unknown <sharmakeshav54126@gmail.com>
2024-08-30 08:09:25 +0530

    auth options corrected

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,6 +5,7 @@ import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
 import { signIn } from "next-auth/react";
 import db from "@/lib/db";
+import { Role } from "@prisma/client";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -31,12 +32,12 @@ export const authOptions: NextAuthOptions = {
         if (existingUser) {
           const passwordValid = await bcrypt.compare(
             credentials.password,
-            existingUser.password
+            existingUser.password || ""
           );
           if (passwordValid) {
             return {
               id: existingUser.id,
-              name: existingUser.fullName,
+              name: existingUser.name,
               email: existingUser.email,
             };
           }
@@ -47,16 +48,18 @@ export const authOptions: NextAuthOptions = {
           const hashedPassword = await bcrypt.hash(credentials.password, 10);
           const newUser = await db.user.create({
             data: {
-              fullName: "Guest",
+              name: "Guest",
               email: credentials.email,
               password: hashedPassword,
+              role: Role.EndUser,
             },
           });
 
           return {
             id: newUser.id,
-            name: newUser.fullName,
+            name: newUser.name,
             email: newUser.email,
+            role: newUser.role,
           };
         } catch (error) {
           console.error("Error creating user:", error);
@@ -93,10 +96,11 @@ export const authOptions: NextAuthOptions = {
         try {
           await db.user.create({
             data: {
-              fullName: profile.name || "New User",
-              email: profile.email || "",
+              name: profile.name,
+              email: profile.email,
               googleId: account.providerAccountId,
               imageUrl: profile.picture,
+              role: Role.EndUser,
             },
           });
           return true;
